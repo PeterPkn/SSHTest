8c23695fdef4997daa64a08c115dcf4dfd00704b
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2020-09-21 11:08:18 +0200

    Remove Tags

--- src/u01_firsttry/Main.java
+++ src/u01_firsttry/Main.java
@@ -2,15 +2,15 @@ package u01_firsttry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(count("ein erster <12z*12)(>/%&(\"=Text"));
+        System.out.println(count(" eins<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>zwei"));
     }
 
     public static int count(String text){
-        text = text.replaceAll("[^A-Za-z<>\\s]","");
+        text = text.replaceAll("[^A-Za-z<>\\s]"," ");
 
-        
+        text = text.replaceAll("<(.*?)>"," ");
 
-        //System.out.println(text);
+        System.out.println(text.trim());
 
         return text.trim().split(" ").length;
     }
