72ce625bdde297e6f9e4830cf847f9613a6c6507
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2020-09-28 12:21:59 +0200

    new Packiage and state diagramm

--- src/u01_firsttry/Main.java
+++ src/u01_firsttry/Main.java
@@ -1,6 +1,8 @@
 package u01_firsttry;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +12,25 @@ public class Main {
     public static int count(String text){
         //<(.*[^\]"(.*)[^\]".*)>4
         //<(.*[^\\]"(.*)[^\\]?".*)*>
+        //<([^>]*([^\\]".*[^\\]")[^>]*)>?
         if(text.trim().isBlank())return 0;
-        text = text.replaceAll("[^A-Za-z<>\\s\"]"," ");
+        text = text.replaceAll("[^A-Za-z<>\\s\"\\\\]"," ");
 
-        //text = text.replaceAll("<(\".*\")>", "");
+        System.out.println(text);
+        Pattern p1 = Pattern.compile("<[^>]*(\\\\\")[^>]*>?");//.matcher(text).group(1).replaceAll(".*", " ");
+
+        Matcher m = p1.matcher(text);
+
+        if(m.find()) {
+            System.out.println(m.group(1));
+            //System.out.println(m.end(1));
+            String text1 = text.trim().substring(0, m.start(1));
+            String text2 = text.trim().substring(m.end(1), text.length()-1);
+
+            text = text1+text2;
+        }
+
+        System.out.println(text);
 
         text = text.replaceAll("<([^>]*(\".*\")[^>]*)>?"," ");
 
