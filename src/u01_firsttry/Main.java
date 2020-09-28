56859b3e96189edd920794d6d3d25acb54620ea1
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2020-09-28 08:05:13 +0200

    Changing REgex for more tests

--- src/u01_firsttry/Main.java
+++ src/u01_firsttry/Main.java
@@ -1,17 +1,27 @@
 package u01_firsttry;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(count(" eins<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>zwei"));
     }
 
     public static int count(String text){
+        //<(.*[^\]"(.*)[^\]".*)>4
+        //<(.*[^\\]"(.*)[^\\]?".*)*>
         if(text.trim().isBlank())return 0;
-        text = text.replaceAll("[^A-Za-z<>\\s]"," ");
+        text = text.replaceAll("[^A-Za-z<>\\s\"]"," ");
 
-        text = text.replaceAll("<(.*?)>"," ");
+        //text = text.replaceAll("<(\".*\")>", "");
+
+        text = text.replaceAll("<([^>]*(\".*\")[^>]*)>?"," ");
 
         System.out.println(text.trim());
+
+        text = text.replaceAll(" +", " ");
+
+        System.out.println(Arrays.toString(text.trim().split(" ")));
 
         return text.trim().split(" ").length;
     }
--- src/u01_firsttry/Tests.java
+++ src/u01_firsttry/Tests.java
@@ -2,33 +2,92 @@ package u01_firsttry;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static u01_firsttry.Main.count;
 
 import org.junit.jupiter.api.Test;
 
 public class Tests {
     @Test
     public void easytests(){
-        assertSame(0,Main.count(""));
-        assertSame(0,Main.count(" "));
-        assertSame(0,Main.count("  "));
+        assertSame(0, count(""));
+        assertSame(0, count(" "));
+        assertSame(0, count("  "));
     }
     @Test
     public void normaltest(){
-        assertSame(1,Main.count("eins"));
-        assertSame(1,Main.count(" eins"));
-        assertSame(1,Main.count(" eins "));
-        assertSame(1,Main.count("  eins    "));
-        assertSame(1,Main.count("eins:"));
-        assertSame(1,Main.count(":eins"));
-        assertSame(1,Main.count(":eins:"));
-        assertSame(1,Main.count(" eins "));
-        assertSame(1,Main.count(" eins : "));
-        assertSame(1,Main.count(": eins :"));
-        assertSame(3,Main.count("ein erster Text"));
-        assertSame(3,Main.count(" ein erster Text "));
-        assertSame(3,Main.count("ein:erster.Text"));
-        assertSame(1,Main.count("a "));
-        assertSame(1,Main.count(" a "));
+        assertSame(1, count("eins"));
+        assertSame(1, count(" eins"));
+        assertSame(1, count(" eins "));
+        assertSame(1, count("  eins    "));
+        assertEquals(1, count("eins"));
+        assertEquals(1, count(" eins"));
+        assertEquals(1, count("eins "));
+        assertEquals(1, count(" eins "));
+        assertEquals(1, count("  eins "));
+        assertEquals(1, count("  eins  "));
+
+        assertEquals(1, count("eins:"));
+        assertEquals(1, count(":eins"));
+        assertEquals(1, count(":eins:"));
+        assertEquals(1, count(" eins  "));
+        assertEquals(1, count("  eins : "));
+        assertEquals(1, count(": eins :"));
+        assertEquals(3, count("ein erster Text"));
+        assertEquals(3, count(" ein erster Text      "));
+        assertEquals(3, count("ein:erster.Text"));
+
+        //vielleicht falsch (Bei mir richtig)
+        assertEquals(1, count("a"));
+        assertEquals(1, count(" a"));
+        assertEquals(1, count("a "));
+        assertEquals(1, count(" a "));
     }
-    
+
+    @Test
+    public void htmltagstest(){
+        //mit html
+        assertEquals(1, count(" eins   <html> "));
+        assertEquals(1, count(" eins   < html> "));
+        assertEquals(1, count(" eins   <html > "));
+        assertEquals(1, count(" eins   < html > "));
+        assertEquals(4, count(" eins  <html> zwei<html>drei <html> vier"));
+        assertEquals(2, count(" eins  <html> zwei "));
+        assertEquals(2, count(" eins  <html>zwei "));
+        assertEquals(2, count(" eins<html> zwei "));
+        assertEquals(2, count(" eins<html>zwei "));
+        assertEquals(2, count(" eins<img alt=\"xxx\" > zwei"));
+        assertEquals(2, count(" eins<img alt=\"xxx yyy\" > zwei"));
+
+        assertEquals(2, count(" eins \"zwei\" "));
+        assertEquals(2, count(" eins\"zwei\" "));
+        assertEquals(2, count(" eins \"zwei\""));
+        assertEquals(3, count(" eins \"zwei\"drei"));
+        assertEquals(3, count(" eins \"zwei\" drei"));
+    }
+
+    @Test
+    public void htmlTricksTest(){
+        // html - trickreich
+        // Achtung: das ist teilweise nicht ganz legales HTML
+        assertEquals(1, count(" eins<html"));
+        assertEquals(2, count(" eins<img alt=\"<bild>\" > zwei"));
+        assertEquals(2, count(" eins<img alt=\"bild>\" > zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild>\" keinwort> zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild>\" src=\"bild.png\">zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild\" keinwort>zwei"));
+
+        assertEquals(1, count(" eins<img alt=\"<bild\" keinwort"));
+        assertEquals(2, count(" eins<img alt=\"<bild\" keinwort> zwei"));
+        assertEquals(1, count(" eins<img alt=\"<bild keinwort> keinwort"));
+        assertEquals(2, count(" eins<img alt=\"<bild keinwort keinwort\">zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild keinwort< keinwort\">zwei"));
+
+        // ganz ganz fies -- \ entwertet das nächste Zeichen
+        assertEquals(2, count(" eins<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild \\\" keinwort<keinwort\" keinwort>zwei"));
+        assertEquals(2, count(" eins<img alt=\"<bild \\\" keinwort keinwort\" keinwort>zwei"));
+
+        assertEquals(4, count(" \\\"null\\\" eins<img alt=\"<bild \\\" keinwort keinwort\"keinwort>zwei \"drei\""));
+    }
+
 }
