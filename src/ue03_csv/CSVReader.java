500e37aee63e5812cd33d4d6345eca80b8d09456
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2020-10-19 20:16:48 +0200

    commit

--- src/ue03_csv/CSVReader.java
+++ src/ue03_csv/CSVReader.java
@@ -1,9 +1,12 @@
 package ue03_csv;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class CSVReader {
     public static ArrayList<String> split(String input){
+        ArrayList<String> cells = new ArrayList<>();
 
+        return cells;
     }
 }
